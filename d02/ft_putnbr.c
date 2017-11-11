d7e6de97562eec127bddae14bac9e4b3561344ad
Albert Han <alberthan92@gmail.com>
2017-11-11 19:14:08 +0900

    Create a function ft_putnbr.c
    The objective of this function is to display all possible int values.

---
+++ d02/ft_putnbr.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
+}
